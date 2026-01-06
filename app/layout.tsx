271d49b48e1ce0c954a23ac70ecde827e57bd95d
Bautista Centorbi <bautii.centorbii@gmail.com>
2026-01-06 19:08:38 -0300

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <Script id="consent-default" strategy="beforeInteractive">
           {`
